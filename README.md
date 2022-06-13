f04819d3bdeef15aba235793b8706a50a1b0aca1
gavinpereira23 <73153006+gavinpereira23@users.noreply.github.com>
2022-06-12 18:35:01 -0700

    commit maps + tiles
    reduce file sizes + add readme
    
    remove all extra tiles

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # tilset-generation
+
+##  Map 1
+![map 1 image](assets/map1.png)
+My map 1 is a basemap with a historical theme in Philadelphia, PA. As such, I uploaded a custom font, and chose beige colors for a more historical script like theme. 
+
+
+## Map 2 
+![map 2 image](assets/map2.png)
+
+Map 2 is a thematic layer of historical districts in Philadelphia, PA. This was from [OpenDataPhilly](https://www.opendataphilly.org/dataset/philadelphia-registered-historic-districts). The background is transparent. 
+
+
+## Map 3
+![map 2 image](assets/map3.png)
+Map 3 is a combination of the first two tilesets, so it displays the historic districts in Philadelphia along with the base map layer from map 1, adding a historical theme. 
+
+## Map 4
+![map 4 image](assets/map4.png)
+
+Map 4 is an edited mapbox layer themed around the colors of the NBA team from Philadelphia, the 76ers, since I am a big sports fan. I chose to use the exact colors from their website, and used the default icons, but chose to display only the icons instead of place names. 
--- main.js
+++ main.js
@@ -19,6 +19,22 @@ mapboxgl.accessToken =
 
 map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
 
+<<<<<<< HEAD
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', //'mapbox://styles/gpereira1/cl49i6d4j000314n3o7ihl0f3',
+    //style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 10, // starting zoom
+    minZoom: 10,
+    maxZoom: 13,
+    center: [-75.165222, 39.952583], // starting center
+    projection: {
+        name: 'albers',
+        parallels: [25, 50]
+    } // starting projection
+
+=======
+>>>>>>> parent of 01bc354c (commit maps + tiles)
 
 
 
@@ -41,4 +57,138 @@ map.on('load', () => { //simplifying the function statement: arrow with brackets
     });
 
 
+<<<<<<< HEAD
+    map.addSource('philly-thematic', {
+        'type': 'raster',
+        'tiles': [
+            'philly-thematic-tiles/{z}/{x}/{y}.png'
+        ],
+        'tileSize': 256
+    });
+
+
+
+
+
+
+
+    map.addSource('philly-combined', {
+        'type': 'raster',
+        'tiles': [
+            'philly-combined-tiles/{z}/{x}/{y}.png'
+        ],
+        'tileSize': 256
+    });
+
+
+
+    map.addLayer({
+        'id': 'philly-combined',
+        'type': 'raster',
+        'layout': {
+            'visibility': 'visible'
+        },
+        'source': 'philly-combined'
+    });
+
+
+    map.addSource('philly-sixers', {
+        'type': 'raster',
+        'tiles': [
+            'philly-sixers-tiles/{z}/{x}/{y}.png'
+        ],
+        'tileSize': 256
+    });
+
+
+
+    map.addLayer({
+        'id': 'philly-sixers',
+        'type': 'raster',
+        'layout': {
+            'visibility': 'visible'
+        },
+        'source': 'philly-sixers'
+    });
+
+
+    map.addLayer({
+        'id': 'philly-thematic',
+        'type': 'raster',
+        'layout': {
+            'visibility': 'visible'
+        },
+        'source': 'philly-thematic'
+    });
+
+
+
+
+
+
+
+    // map.on('mousemove', (e) => {
+    //         // `e.point` is the x, y coordinates of the `mousemove` event
+    //         // relative to the top-left corner of the map.
+    //         JSON.stringify(e.point) +
+    //         '<br />' +
+    //         // `e.lngLat` is the longitude, latitude geographical position of the event.
+    //         console.log(JSON.stringify(e.lngLat.wrap()));
+    // });
+
+
+});
+
+
+map.on('idle', () => {
+
+
+    // Enumerate ids of the layers.
+    const toggleableLayerIds = ['philly-base', 'philly-thematic', 'philly-combined', 'philly-sixers'];
+
+    // Set up the corresponding toggle button for each layer.
+    for (const id of toggleableLayerIds) {
+        // Skip layers that already have a button set up.
+        if (document.getElementById(id)) {
+            continue;
+        }
+
+        // Create a link.
+        const link = document.createElement('a');
+        link.id = id;
+        link.href = '#';
+        link.textContent = id;
+        link.className = 'active';
+
+        // Show or hide layer when the toggle is clicked.
+        link.onclick = function(e) {
+            const clickedLayer = this.textContent;
+            e.preventDefault();
+            e.stopPropagation();
+
+            const visibility = map.getLayoutProperty(
+                clickedLayer,
+                'visibility'
+            );
+
+            // Toggle layer visibility by changing the layout object's visibility property.
+            if (visibility === 'visible') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+                this.className = '';
+            } else {
+                this.className = 'active';
+                map.setLayoutProperty(
+                    clickedLayer,
+                    'visibility',
+                    'visible'
+                );
+            }
+        };
+
+        const layers = document.getElementById('menu');
+        layers.appendChild(link);
+
+    }
+=======
+>>>>>>> parent of 01bc354c (commit maps + tiles)
 });
